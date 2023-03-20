5d6e682727a7ca47903df5d6a4cde106249b62bd
Jaime Tenorio <jaimeteb@gmail.com>
2023-03-20 20:31:32 +0100

    Refactor

--- jaipy/model.py
+++ jaipy/model.py
@@ -26,87 +26,77 @@ ANCHORS = (
 ).T
 
 
-def convolutional(input_layer, filters_shape, downsample=False, activate=True, bn=True):
-    if downsample:
-        input_layer = ZeroPadding2D(((1, 0), (1, 0)))(input_layer)
-        padding = "valid"
-        strides = 2
-    else:
-        strides = 1
-        padding = "same"
+def Convolutional(layer, filters_shape, activate=True, bn=True):
+    strides = 1
+    padding = "same"
 
-    conv = Conv2D(
+    layer = Conv2D(
         filters=filters_shape[-1],
         kernel_size=filters_shape[0],
         strides=strides,
         padding=padding,
         use_bias=not bn,
-        kernel_regularizer=l2(0.0005),
+        kernel_regularizer=l2(0.000_5),
         kernel_initializer=tf.random_normal_initializer(stddev=0.01),
         bias_initializer=tf.constant_initializer(0.0),
-    )(input_layer)
-    if bn:
-        conv = BatchNormalization()(conv)
-    if activate:
-        conv = LeakyReLU(alpha=0.1)(conv)
+    )(layer)
+    layer = BatchNormalization()(layer) if bn else layer
+    layer = LeakyReLU(alpha=0.1)(layer) if activate else layer
 
-    return conv
+    return layer
 
 
-def upsample(input_layer):
+def UpSample(layer):
     return tf.image.resize(
-        input_layer,
-        (input_layer.shape[1] * 2, input_layer.shape[2] * 2),
+        layer,
+        (
+            layer.shape[1] * 2,
+            layer.shape[2] * 2,
+        ),
         method="nearest",
     )
 
 
-def darknet19_tiny(input_data):
-    input_data = convolutional(input_data, (3, 3, 3, 16))
-    input_data = MaxPool2D(2, 2, "same")(input_data)
-    input_data = convolutional(input_data, (3, 3, 16, 32))
-    input_data = MaxPool2D(2, 2, "same")(input_data)
-    input_data = convolutional(input_data, (3, 3, 32, 64))
-    input_data = MaxPool2D(2, 2, "same")(input_data)
-    input_data = convolutional(input_data, (3, 3, 64, 128))
-    input_data = MaxPool2D(2, 2, "same")(input_data)
-    input_data = convolutional(input_data, (3, 3, 128, 256))
-    route_1 = input_data
-    input_data = MaxPool2D(2, 2, "same")(input_data)
-    input_data = convolutional(input_data, (3, 3, 256, 512))
-    input_data = MaxPool2D(2, 1, "same")(input_data)
-    input_data = convolutional(input_data, (3, 3, 512, 1024))
+def YOLOv3_tiny(layer, NUM_CLASS):
+    # darknet19_tiny(layer):
+    layer = Convolutional(layer, (3, 3, 3, 16))
+    layer = MaxPool2D(2, 2, "same")(layer)
+    layer = Convolutional(layer, (3, 3, 16, 32))
+    layer = MaxPool2D(2, 2, "same")(layer)
+    layer = Convolutional(layer, (3, 3, 32, 64))
+    layer = MaxPool2D(2, 2, "same")(layer)
+    layer = Convolutional(layer, (3, 3, 64, 128))
+    layer = MaxPool2D(2, 2, "same")(layer)
+    layer = Convolutional(layer, (3, 3, 128, 256))
+    layer_scale = layer
+    layer = MaxPool2D(2, 2, "same")(layer)
+    layer = Convolutional(layer, (3, 3, 256, 512))
+    layer = MaxPool2D(2, 1, "same")(layer)
+    layer = Convolutional(layer, (3, 3, 512, 1024))
 
-    return route_1, input_data
+    layer = Convolutional(layer, (1, 1, 1024, 256))
+    layer_lobj_branch = Convolutional(layer, (3, 3, 256, 512))
 
-
-def YOLOv3_tiny(input_layer, NUM_CLASS):
-    # After the input layer enters the Darknet-53 network, we get three branches
-    route_1, conv = darknet19_tiny(input_layer)
-
-    conv = convolutional(conv, (1, 1, 1024, 256))
-    conv_lobj_branch = convolutional(conv, (3, 3, 256, 512))
-
-    # conv_lbbox is used to predict large-sized objects , Shape = [None, 26, 26, 255]
-    conv_lbbox = convolutional(
-        conv_lobj_branch, (1, 1, 512, 3 * (NUM_CLASS + 5)), activate=False, bn=False
+    # layer_lbbox is used to predict large-sized objects , Shape = [None, 26, 26, 255]
+    layer_lbbox = Convolutional(
+        layer_lobj_branch, (1, 1, 512, 3 * (NUM_CLASS + 5)), activate=False, bn=False
     )
 
-    conv = convolutional(conv, (1, 1, 256, 128))
+    layer = Convolutional(layer, (1, 1, 256, 128))
     # upsample here uses the nearest neighbor interpolation method, which has the advantage that the
     # upsampling process does not need to learn, thereby reducing the network parameter
-    conv = upsample(conv)
+    layer = UpSample(layer)
 
-    conv = tf.concat(  # pylint: disable=unexpected-keyword-arg,no-value-for-parameter
-        [conv, route_1], axis=-1
+    layer = tf.concat(  # pylint: disable=unexpected-keyword-arg,no-value-for-parameter
+        [layer, layer_scale], axis=-1
     )
-    conv_mobj_branch = convolutional(conv, (3, 3, 128, 256))
-    # conv_mbbox is used to predict medium size objects, shape = [None, 13, 13, 255]
-    conv_mbbox = convolutional(
-        conv_mobj_branch, (1, 1, 256, 3 * (NUM_CLASS + 5)), activate=False, bn=False
+    layer_mobj_branch = Convolutional(layer, (3, 3, 128, 256))
+    # layer_mbbox is used to predict medium size objects, shape = [None, 13, 13, 255]
+    layer_mbbox = Convolutional(
+        layer_mobj_branch, (1, 1, 256, 3 * (NUM_CLASS + 5)), activate=False, bn=False
     )
 
-    return [conv_mbbox, conv_lbbox]
+    return [layer_mbbox, layer_lbbox]
 
 
 def decode(conv_output, NUM_CLASS, i=0):
@@ -188,4 +178,4 @@ def Create_Yolov3(input_size=416, channels=3, training=False):
     return YoloV3
 
 
-net = Create_Yolov3()
+net = Create_Yolov3(training=True)
